6e2a744fbe51abbb045c6357ceda2e148dc8ff07
rgetsy <48496256+rgetsy@users.noreply.github.com>
2019-04-04 13:28:37 -0500

    after jobs and bootstrap done

--- portfolio-project/blogs/models.py
+++ portfolio-project/blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField (max_length=100)
+    pubdte = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField (upload_to='images/')
+#def get_now():
+#    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
--- portfolio-project/portfolio/urls.py
+++ portfolio-project/portfolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
+import jobs.views    #added 4/1
 urlpatterns = [
+    path('', jobs.views.home, name='home'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
